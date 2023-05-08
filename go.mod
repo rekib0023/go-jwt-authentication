da9f27fda35c1b59327ec8aad435c90ae4533d93
rekib0023 <rkb.ra0025@gmail.com>
2023-05-08 15:46:09 +0530

    added cors middleware

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-playground/validator/v10 v10.11.2
 	github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"go-jwt-authentication/helpers"
 	"go-jwt-authentication/middleware"
 	"go-jwt-authentication/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,6 +19,15 @@ func main() {
 	database.Connect()
 
 	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Use(gin.Logger())
 	routes.AuthRoutes(router)
 	router.Use(middleware.AuthMiddleware())
